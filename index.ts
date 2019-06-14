f668d9681bb09097e1e87a13241db8331fc421de
Svensson <Per.Svensson@dnvgl.com>
2019-06-14 09:30:34 +0200

    move and http handler

---
+++ index.ts
@@ -0,0 +1,21 @@
+import { createServer } from "http";
+import * as socketio from "socket.io";
+
+const server = createServer(httpHandler);
+const io = socketio(server);
+
+function httpHandler(_, res){
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write("Hello World");
+        res.end("\n");
+};
+
+server.listen(3009, () => {
+    console.log("listening on *:3009");
+});
+
+io.on("connection", (socket) => {
+    socket.on("action_event", (msg) => {
+        socket.emit("action_event", msg);
+    });
+});
