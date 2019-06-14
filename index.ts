be6982c721438d093a5b91fa3b2e79e17aa39a33
Svensson <Per.Svensson@dnvgl.com>
2019-06-14 14:18:58 +0200

    azure configs

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ const port = process.env.port || 1337;
 const server = createServer(httpHandler);
 const io = socketio(server, {
     perMessageDeflate: false
-  });
+});
 
-function httpHandler(_, res){
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.write("Hello World");
-        res.end("\n");
-};
+function httpHandler(_, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Hello World");
+    res.end("\n");
+}
 
 server.listen(port, () => {
     console.log(`listening on *:${port}`);
